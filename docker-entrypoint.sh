33ed209993ee0fd435f6a4e3239280b1012388db
Redy <19371836+insoxin@users.noreply.github.com>
2022-04-02 15:45:06 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,15 @@
-FROM debian
-RUN apt-get update
-RUN apt install git wget curl sudo -y
-RUN git clone https://github.com/insoxin/imaotai.git
-RUN mkdir /root/imaotai
-RUN ls
-RUN cp -r /imaotai/* /root/imaotai
-ENV QR_JDC=/imaotai
-EXPOSE 1499
-ENTRYPOINT ["/imaotai/docker-entrypoint.sh"]
+#!/bin/bash
+set -e
+
+if [ ! -s ${imaotai}/config/config.json ]; then
+  echo "检测到config配置目录下不存在config.json，从示例文件复制一份用于初始化...\n"
+  cp -fv ${imaotai}/config/config.json ${imaotai}/config/config.json
+  cp -fv ${imaotai}/config/sendNotify.js ${imaotai}/config/sendNotify.js
+fi
+
+if [ -s ${imaotai}/config/config.json ]; then
+  echo "检测到config配置目录下存在config.json，即将启动...\n"
+
+fi
+
+exec "$@"
